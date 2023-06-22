f0218c332665bfc1a3de59753c48fa5872532ba2
Mayurgaike <mayurgaike486@gmail.com>
2023-06-22 23:48:34 +0530

    making it pwa

---
+++ Images/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('weather-app-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/Images/',
+          '/manifest.json',
+          '/service-worker.js'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
